f8f1ca820bf3af20a528a60823818011545558f1
Vipin Gupta <vipin0900@gmail.com>
2020-06-14 13:56:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import time
 import requests
 import os
 
+EDIT_TIME=config.EDIT_TIME
+
 
 def download_link(link,file_name,prev_message):
   time1=time.time()
@@ -93,7 +95,7 @@ def progress_bar_f(current,total,prev_message,time1,direction):
     if total==current: 
         txt_to_send=direction+"ed : complete 100% ["+k2+"]\n"+k3+pro_bar_str
         txt_to_send=txt_to_send+"\nAvg. Transfer Speed :"+k4+ps
-        await asyncio.sleep(3)
+        await asyncio.sleep(EDIT_TIME)
         prev_message=app.edit_message_text(
             chat_id=prev_message.chat.id,
             message_id=prev_message.message_id,
